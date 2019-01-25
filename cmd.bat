9c11387f037450bc52262fe39474f3f0ef2782a2
plajuan <pla.juan@gmail.com>
2019-01-25 14:30:56 -0300

    Sass

--- cmd.bat
+++ cmd.bat
@@ -4,3 +4,9 @@ npm install font-awesome@4.7.0 --save
 npm install bootstrap-social@5.1.1 --save
 sudo apt install node-less
 lessc styles.less styles.css
+
+// save-dev significa que a biblioteca será adicionada automaticamente no package.json
+ npm install --save-dev node-sass@4.7.2
+
+//executar este comando sempre que o CSS for alterado
+ npm run scss
