1cbac0bab64c71914ddde7edbaaf8b2298a19c82
plajuan <pla.juan@gmail.com>
2019-01-27 22:27:09 -0300

    NPM Scripts Part 2

--- cmd.bat
+++ cmd.bat
@@ -1,7 +1,11 @@
 npm install bootstrap@4.0.0 --save
 npm install jquery@3.3.1 popper.js@1.12.9 --save
+
+//gráficos e botões
 npm install font-awesome@4.7.0 --save
 npm install bootstrap-social@5.1.1 --save
+
+//css pre-processors
 sudo apt install node-less
 lessc styles.less styles.css
 
@@ -14,3 +18,14 @@ lessc styles.less styles.css
  //Não precisa mais rodar o comando acima.
  //package.json agora vai rodar automaticamente usando o onchange
  npm install --save-dev onchange@3.3.0 parallelshell@3.0.2^C
+
+//pacote para distribuição de binários
+npm install --save-dev rimraf@2.6.2
+//copiar arquivos
+sudo npm -g install copyfiles@2.0.0
+//minimizar figuras
+sudo npm -g install imagemin-cli@3.0.0
+//complementar
+npm install --save-dev usemin-cli@0.5.1 cssmin@0.4.3 uglifyjs@2.4.11 htmlmin@0.0.7
+
+npm run build
