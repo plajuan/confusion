debadf29195133212753736ef1998e613d5f4e49
plajuan <pla.juan@gmail.com>
2019-01-29 20:54:57 -0300

    Grunt Part 1

--- cmd.bat
+++ cmd.bat
@@ -29,3 +29,17 @@ sudo npm -g install imagemin-cli@3.0.0
 npm install --save-dev usemin-cli@0.5.1 cssmin@0.4.3 uglifyjs@2.4.11 htmlmin@0.0.7
 
 npm run build
+
+//grunt
+sudo npm install -g grunt-cli@1.2.0
+npm install grunt@1.0.2 --save-dev
+npm install grunt-sass@2.1.0 --save-dev
+npm install time-grunt@1.4.0 --save-dev
+npm install jit-grunt@0.10.0 --save-dev
+/*
+Grunt module for SCSS to CSS conversion. 
+The time-grunt module generates time statistics about how much time each task consumes, 
+and jit-grunt enables us to include the necessary downloaded Grunt modules when needed for the tasks.
+*/
+npm install grunt-contrib-watch@1.0.0 --save-dev
+npm install grunt-browser-sync@2.2.0 --save-dev
