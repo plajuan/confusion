6202dc6de4e946b74d7cb3ccf0b7721622ff4dfe
plajuan <pla.juan@gmail.com>
2019-01-29 21:32:37 -0300

    Grunt Part 2

--- cmd.bat
+++ cmd.bat
@@ -43,3 +43,14 @@ and jit-grunt enables us to include the necessary downloaded Grunt modules when 
 */
 npm install grunt-contrib-watch@1.0.0 --save-dev
 npm install grunt-browser-sync@2.2.0 --save-dev
+
+npm install grunt-contrib-copy@1.0.0 --save-dev
+npm install grunt-contrib-clean@1.1.0 --save-dev
+npm install grunt-contrib-imagemin@2.0.1 --save-dev
+
+npm install grunt-contrib-concat@1.0.1 --save-dev
+ npm install grunt-contrib-cssmin@2.2.1 --save-dev
+ npm install grunt-contrib-htmlmin@2.4.0 --save-dev
+ npm install grunt-contrib-uglify@3.3.0 --save-dev
+ npm install grunt-filerev@2.3.1 --save-dev
+ npm install grunt-usemin@3.1.1 --save-dev
